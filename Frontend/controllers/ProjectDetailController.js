5e3baae05cd60ba058af8aa0b6402b1e8f71df2d
ceparent <ceparent@ceparent.info>
2015-01-02 00:11:53 -0500

    Project detail

--- Frontend/controllers/ProjectDetailController.js
+++ Frontend/controllers/ProjectDetailController.js
@@ -3,9 +3,32 @@ var app = angular.module('ceparent.projectDetail', [])
 
 
         $scope.project = {
-            "id" : 1
-
-
+            "id" : 1,
+            "name": "ceparent.info",
+            "image":"img/ceparentInfo.PNG",
+            "description": "my personal web site",
+            "features":[
+                "One-page AngularJs client",
+                "Java web api backend",
+                "Fully responsive Boostrap3 design",
+                "OAuth2 token-based authentification",
+                "Admin cms panel"
+            ],
+            "technologies":[
+                "AngularJS",
+                "DropWizard(Jetty, Jersey, Jackson, LiquiBase, Hibernate)",
+                "Bootstrap 3"
+            ],
+            "links":[
+                {
+                    "name":"Website",
+                    "url":"http://ceparent.info"
+                },
+                {
+                    "name":"Github repo (client only)",
+                    "url":"https://github.com/ceparent/ceparent-client"
+                }
+            ]
         }
 
 
